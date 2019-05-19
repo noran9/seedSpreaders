972cdbe0cf9ca259d3ca8289a649b27705fbb323
noran9 <nora.nikoloska@hotmail.com>
2019-05-19 07:53:36 +0200

    Updates

---
+++ precache-manifest.7d7c83b18792aeef974e6ac9ec68762e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0595032532eda7c867fb3a11cdc96521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93404f294b531dc75166",
+    "url": "/static/css/main.18b23b37.chunk.css"
+  },
+  {
+    "revision": "54d0b29fee33e582b00f",
+    "url": "/static/js/2.d616d5df.chunk.js"
+  },
+  {
+    "revision": "93404f294b531dc75166",
+    "url": "/static/js/main.870c3319.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bd6450748e92387a963bd91abea6772",
+    "url": "/static/media/daisy.7bd64507.jpg"
+  },
+  {
+    "revision": "8c8ddbeb092a9c7601f3cd7d0926a31e",
+    "url": "/static/media/lavender.8c8ddbeb.jpg"
+  },
+  {
+    "revision": "d0539ebf44b6871f96615f97f8947821",
+    "url": "/static/media/logo.d0539ebf.png"
+  },
+  {
+    "revision": "f8d763ae92a777db6e0ba67a805b53e4",
+    "url": "/static/media/marigold.f8d763ae.jpg"
+  }
+]);
